2c6fa666720f4e7cd70858cf59f590592388947a
vmathmachine <31744299+vmathmachine@users.noreply.github.com>
2022-04-07 21:51:18 -0400

    Create Cpx2.java

---
+++ ComplexNumberLibrary/src/complexnumbers/Cpx2.java
@@ -0,0 +1,22 @@
+package complexnumbers;
+
+/**
+ * An additional class for more complicated, special functions.
+ * 
+ * <br> Such functions currently include the Gamma function, as well as several related functions such as the factorial and the digamma
+ * function.
+ * 
+ * <br> In later versions, I plan on implementing functions related to the error function, the Riemmann Zeta function, the exponential
+ * integral, polylogarithms, and elliptic integrals.  All of said functions have been built, but they have yet to be rigorously tested.
+ * Also, the Elliptic integral of the third kind is extremely in the beta stages, as while I know how to compute it, I haven't the foggiest
+ * idea how to remove any of its branch cuts.
+ * 
+ * <br> I also have plans to implement the Bessel functions, the incomplete gamma function, the Hurwitz zeta function, the Lambert W function
+ * (also known as the product logarithm).  However, none of said functions have been built.
+ * 
+ * @author Math Machine
+ * @version 1.0.0
+ */
+public class Cpx2 extends Cpx {
+  
+}
