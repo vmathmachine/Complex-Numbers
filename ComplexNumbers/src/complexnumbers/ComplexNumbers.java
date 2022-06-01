070d02a06a422438e1598da0050a7ed7e6e4c92c
vmathmachine <31744299+vmathmachine@users.noreply.github.com>
2022-06-01 15:24:40 -0400

    Update ComplexNumbers.java

--- ComplexNumbers/src/complexnumbers/ComplexNumbers.java
+++ ComplexNumbers/src/complexnumbers/ComplexNumbers.java
@@ -19,7 +19,7 @@ public class ComplexNumbers {
 	/**
 	 * The current version of the Library.
 	 */
-	public final static String VERSION = "1.0.1";
+	public final static String VERSION = "1.0.2";
 	
 	/**
 	 * Returns the version of the Library.
